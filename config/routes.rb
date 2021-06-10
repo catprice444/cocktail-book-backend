024c593c6917309c00b751fd03229be7fa0e0da0
catprice444 <catprice444@gmail.com>
2021-06-09 20:53:27 -0400

    updated controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :cocktails
-  resources :alcohols
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do 
+    namespace :v1 do 
+      resources :cocktails
+      resources :alcohols
+    end 
+  end 
+
 end
