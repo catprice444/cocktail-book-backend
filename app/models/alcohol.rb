54b48374140ee0c6ef54dfc4ef7b4af25a8bcc71
catprice444 <catprice444@gmail.com>
2021-06-09 20:43:46 -0400

    added more validations

--- app/models/alcohol.rb
+++ app/models/alcohol.rb
@@ -1,4 +1,5 @@
 class Alcohol < ApplicationRecord
     has_many :cocktails
     validates :name, presence: true 
+    validates :preference, :less_than_or_equal_to => 10
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,5 @@
 class Cocktail < ApplicationRecord
     belongs_to :alcohol 
     validates :name, :recipe, :ingredients, presence: true 
-end
+    validates :rating, allow_nil: true, :less_than_or_equal_to => 5
+end 
