9d639ffa4f445c57f8375af171348fb512c80ec1
catprice444 <catprice444@gmail.com>
2021-06-12 20:06:08 -0400

    basic cocktail controller

--- app/controllers/api/v1/alcohols_controller.rb
+++ app/controllers/api/v1/alcohols_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::AlcoholsController < ApplicationController
+    def index 
+    end 
+
+    def create 
+    end 
+
+    def show 
+    end 
+
+    def edit 
+    end 
+
+    private 
+    def alcohol_params 
+    end 
+
 end
--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::CocktailsController < ApplicationController
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def create
+    end 
+
+    def edit 
+    end 
+
+    def destroy
+    end 
+
+    private
+    def cocktail_params
+        params.require(:cocktail).permit(:name, :recipe, :ingredients, :rating, :alcohol)
+    end 
+
 end
