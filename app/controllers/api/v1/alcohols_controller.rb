95bdba43d0e472518f9b23994ac5127107008648
catprice444 <catprice444@gmail.com>
2021-06-12 20:26:28 -0400

    built out basic controller functions

--- app/controllers/api/v1/alcohols_controller.rb
+++ app/controllers/api/v1/alcohols_controller.rb
@@ -1,14 +1,26 @@
 class Api::V1::AlcoholsController < ApplicationController
     def index 
+        @alcohols = Alcohol.all 
+        render json: @alcohols
     end 
 
     def create 
+        @alcohol = Alcohol.new(alcohol_params)
+        if @alcohol.save 
+            render json: @account 
+        else 
+            render json: (error: "Error creating Alcohol")
+        end 
     end 
 
     def show 
+        @alcohol = Alcohol.find(params[:id])
+        render json: @alcohol
     end 
 
     def edit 
+        @alcohol = Alcohol.find(params[:id])
+        if 
     end 
 
     private 
--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,17 +1,35 @@
 class Api::V1::CocktailsController < ApplicationController
     def index 
+        @cocktails = Cocktail.all
+        render json: @cocktails
     end 
 
     def show 
+        @cocktail = Cocktail.find(params[:id])
+        render json: @cocktail
     end 
 
     def create
+        @cocktail = Cocktail.new(cocktail_params)
+        if @cocktail.save 
+            render json: @cocktail
+        else 
+            render json: (error: "Error making Cocktail")
+        end 
     end 
 
     def edit 
+        @cocktail = Cocktail.find(params[:id])
+        if @cocktail.save
+            render json: @cocktail
+        else 
+            render json: (error: "Error saving Cocktail")
+        end 
     end 
 
     def destroy
+        @cocktail = Cocktail.find(params[:id])
+        @cocktail.destroy
     end 
 
     private
