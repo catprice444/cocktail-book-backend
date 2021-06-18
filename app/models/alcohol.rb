ff7d862a274607f100ec1329287889a2a663fa98
catprice444 <catprice444@gmail.com>
2021-06-18 00:06:57 -0400

    added basic validations

--- app/models/alcohol.rb
+++ app/models/alcohol.rb
@@ -1,3 +1,4 @@
 class Alcohol < ApplicationRecord
     has_many :cocktails
+    validates :name, :website, presence: true
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,4 @@
 class Cocktail < ApplicationRecord
     belongs_to :alcohol 
-    # validates :name, :recipe, :ingredients, presence: true 
-    # validates_numericality_of :rating, :allow_nil => true, :less_than_or_equal_to => 5
+    validates :name, :recipe, :ingredients, :rating, presence: true 
 end 
